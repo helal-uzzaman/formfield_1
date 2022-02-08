c3a4240c615910e8c86f64952d6ac4fa0906d3ce
helal-uzzaman <hasib.helaluzzaman@gmail.com>
2022-02-08 12:20:37 +0600

    almost done.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _emailFocus = FocusNode();
+  final _passwordFocus = FocusNode();
+  var _obscureText = true;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
+    super.dispose();
+  }
+
+  void _toggleObscureText() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,43 +35,106 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      // hintText: 'Name',
-                      label: Text('user Name'),
-                    ),
-                    textInputAction: TextInputAction.next,
-                    onEditingComplete: () => {
-                      print('onEditing complete Text form field'),
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.email),
-                      // hintText: 'Name',
-                      label: Text('user Email'),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.next,
-                    onEditingComplete: () => {
-                      print('onEditing complete Text form field'),
-                    },
-                  ),
-                  TextFormField(),
-                ],
+        body: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                ' User Login ',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              onChanged: () {
-                print('form onchanged.');
-              },
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                // autovalidateMode: AutovalidateMode.always,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.person),
+                        label: Text('user Name'),
+                      ),
+                      textInputAction: TextInputAction.next,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Empty string can not be used.';
+                        }
+                        return null;
+                      },
+                      onEditingComplete: () => {
+                        _emailFocus.requestFocus(),
+                        print('onEditing complete Text form field'),
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.email),
+                        label: Text('user Email'),
+                      ),
+                      focusNode: _emailFocus,
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      onEditingComplete: () => {
+                        _passwordFocus.requestFocus(),
+                        print('onEditing complete Text form field'),
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Empty string can not be used.';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          icon: const Icon(Icons.lock),
+                          label: const Text('Your password'),
+                          suffixIcon: IconButton(
+                            onPressed: _toggleObscureText,
+                            icon: const Icon(Icons.remove_red_eye),
+                          )),
+                      obscureText: _obscureText,
+                      focusNode: _passwordFocus,
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      onEditingComplete: () => {
+                        print('onEditing complete Text form field'),
+                      },
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {_formKey.currentState!.validate()},
+                      style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20))),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            gradient: const LinearGradient(colors: [
+                              Color.fromARGB(255, 54, 244, 155),
+                              Color.fromARGB(255, 163, 255, 59)
+                            ]),
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Container(
+                          height: 36,
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                                fontSize: 24, fontStyle: FontStyle.italic),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
